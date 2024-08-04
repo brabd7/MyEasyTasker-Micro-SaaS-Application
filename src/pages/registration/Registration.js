45072ea34c9a663e420e84e3dea82c2755e439a2
Itachack <br.abd7@gmail.com>
2024-08-04 11:15:46 +0200

    Ajout de l'icone du site, du fichier .env et début de la mise en place du système d'inscription

--- src/pages/registration/Registration.js
+++ src/pages/registration/Registration.js
@@ -3,15 +3,29 @@ import InputAuth from '../../components/InputAuth';
 import { Link } from 'react-router-dom';
 
 const Registration = (props) => {
+
+    // Gérer la soumission du formulaire
+    const handleSubmit = (event) => {
+        
+        // Récupérer les valeurs
+        const username = document.querySelector('.inputUsername').value;
+        const email = document.querySelector('.inputEmail').value;
+        const password = document.querySelector('.inputPassword').value;
+        const confirmPassword = document.querySelector('.inputConfirmPassword').value;
+
+        // Processus de vérification des entrées
+        
+    }
+
     return (
         <div id='Registration'>
             <div className='form'>
-                <InputAuth icon="fa-regular fa-user" type="text" placeholder="Nom d'utilisateur" className="inputUserName" />
+                <InputAuth icon="fa-regular fa-user" type="text" placeholder="Nom d'utilisateur" className="inputUsername" />
                 <InputAuth icon="fa-regular fa-envelope" type="email" placeholder="Adresse électronique" className="inputEmail" />
                 <InputAuth icon="fa-solid fa-lock" type="password" placeholder="Mot de passe" className="inputPassword" />
                 <InputAuth icon="fa-solid fa-lock" type="password" placeholder="Confirmation du mot de passe" className="inputConfirmPassword" />
             </div>
-            <button className='buttonSubmitAuthentification'>Je m'inscris</button>
+            <button className='buttonSubmitAuthentification' onClick={handleSubmit}>Je m'inscris</button>
             <Link to='/'>Me connecter</Link>
         </div>
     );
