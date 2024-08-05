1603a4091bad032ac6130b0cfaaff85e086898f4
Itachack <br.abd7@gmail.com>
2024-08-05 09:40:49 +0200

    Renforcement et factorisation des fonctions de vérification d'entrées utilisateurs notamment les attaques XSS

--- src/pages/registration/Registration.js
+++ src/pages/registration/Registration.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import InputAuth from '../../components/InputAuth';
 import { Link } from 'react-router-dom';
-import { validateAndSanitizeFormData } from '../../utils/form';
+import { inputCleaningProcess, validateInputEmail } from '../../utils/form';
 
 const Registration = (props) => {
 
@@ -10,12 +10,22 @@ const Registration = (props) => {
         
         // Récupérer les valeurs
         const username = document.querySelector('.inputUsername').value;
-        const email = document.querySelector('.inputEmail').value;
         const password = document.querySelector('.inputPassword').value;
         const confirmPassword = document.querySelector('.inputConfirmPassword').value;
 
-        // Processus de vérification des entrées
-        console.log(validateAndSanitizeFormData(username, email, password, confirmPassword))
+        // Vérifier la validité de l'email
+        if (validateInputEmail(document.querySelector('.inputEmail').value))
+        {
+            const email = document.querySelector('.inputEmail').value;
+            document.querySelector('.error').style.display = "none";
+        }
+        else 
+        {
+            // Afficher l'erreur
+            document.querySelector('.error').style.display = "block";
+        }
+
+        // Processus de nettoyage des entrées
     }
 
     return (
@@ -26,6 +36,7 @@ const Registration = (props) => {
                 <InputAuth icon="fa-solid fa-lock" type="password" placeholder="Mot de passe" className="inputPassword" />
                 <InputAuth icon="fa-solid fa-lock" type="password" placeholder="Confirmation du mot de passe" className="inputConfirmPassword" />
             </div>
+            <p className='error'>L'adresse électronique est invalide !</p>
             <button className='buttonSubmitAuthentification' onClick={handleSubmit}>Je m'inscris</button>
             <Link to='/'>Me connecter</Link>
         </div>
--- src/utils/form.js
+++ src/utils/form.js
@@ -10,52 +10,34 @@ function escapeHtml(html)
 }
 
 // Fonction pour valider les entrées de base
-function validateInput(value, type) 
+function validateInputString(value, type) 
 {
     // Validation de base pour les chaînes de caractères
     if (typeof value !== 'string' || value.trim().length === 0) {
         return false;
     }
-    if (type === 'email') {
-        // Validation simple d'une adresse email
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailPattern.test(value);
-    }
     return true;
 }
 
-// Fonction principale pour obtenir, valider et nettoyer les valeurs du formulaire
-export function validateAndSanitizeFormData(username, email, password, confirmPassword) 
+// Fonction pour valider les emails
+export function validateInputEmail(value)
 {
-    // Nettoyage des valeurs pour éviter les attaques XSS
-    const sanitizedUsername = escapeHtml(username);
-    const sanitizedEmail = escapeHtml(email);
-    const sanitizedPassword = escapeHtml(password);
-    const sanitizedConfirmPassword = escapeHtml(confirmPassword);
+    // Validation simple d'une adresse email
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(value);
+}
 
-    // Validation des valeurs
-    if (!validateInput(sanitizedUsername)) {
-        console.error('Le nom d\'utilisateur est requis et ne peut pas être vide.');
-        return;
-    }
-    if (!validateInput(sanitizedEmail, 'email')) {
-        console.error('Adresse email invalide.');
-        return;
-    }
-    if (!validateInput(sanitizedPassword)) {
-        console.error('Le mot de passe est requis.');
-        return;
-    }
-    if (sanitizedPassword !== sanitizedConfirmPassword) {
-        console.error('Les mots de passe ne correspondent pas.');
-        return;
+// Fonction de processus de nettoyage d'entrée
+export function inputCleaningProcess(elements)
+{
+    const inputs = {};
+    
+    // Pour chaque clé dans l'objet 'elements'
+    for (const key in elements) {
+        const escapeElement = escapeHtml(elements[key]);
+
+        console.log(typeof escapeElement)
     }
 
-    // Retourner les valeurs nettoyées et validées
-    return {
-        username: sanitizedUsername,
-        email: sanitizedEmail,
-        password: sanitizedPassword,
-        confirmPassword: sanitizedConfirmPassword
-    };
+    return inputs;
 }
